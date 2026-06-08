4508ac2528b4d8e41fe9d6533bf7a71f197e20e5
Josephine Mundt <josephinemundt1297@gmail.com>
2026-06-08 15:59:31 +0200

    feat: initialize React project with Vite, add basic app structure and styles
    - Created package.json with dependencies and scripts for development and build.
    - Added App component with useRef hook for input focus management.
    - Included App.css for styling components and layout.
    - Set up index.css with CSS variables for theming.
    - Configured main.jsx for rendering the App component.
    - Established Vite configuration for React and Babel integration.

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useRef, useState } from "react";
+import "./App.css";
+
+function App() {
+  // Hier die Hooks use...
+  const inputingRef = useRef(null);
+  // Hier meine Funktionen für die Logik
+  // function namenAufrufen() {
+  //   console.log("Josi");
+  // }
+  function handleFocus() {
+    console.log(inputingRef.current);
+
+    inputingRef.current.focus();
+  }
+
+  function clearFocus() {
+    console.log("Eingabe leeren und fokussieren");
+    inputingRef.current.focus();
+    inputingRef.current.value = "";
+  }
+
+  return (
+    <>
+      <h1>useRef Playground</h1>
+      <form>
+        <input
+          ref={inputingRef}
+          type="text"
+          placeholder="vollständiger Name"
+        ></input>
+        <button onClick={handleFocus} type="button">
+          Fokus setzen
+        </button>
+        <button onClick={clearFocus} type="button">
+          Eingabe leeren und fokussieren
+        </button>
+        <div>Werte ohne Re-Render speichern</div>
+        <div>Vergleich von useRef und State</div>
+      </form>
+    </>
+  );
+}
+
+export default App;
